f7b90f8056694cabaa83fe490554dd644f462ceb
David Pochik <pochik.1@buckeyemail.osu.edu>
2025-04-17 17:07:23 -0400

    More updates on the lat/long script

--- analysis_scripts/latitude_longitude_grid.py
+++ analysis_scripts/latitude_longitude_grid.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.io as pio
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_error
 import sys
 from sklearn.pipeline import Pipeline 
-from sklearn.preprocessing import StandardScaler
 sns.set_style("whitegrid")
 
 # Specify directory string in which you have the data file stored
@@ -17,13 +18,13 @@ user_specified_directory = '/mnt/d/Erdos_DIR/project_data_TAWNYFILE_DIR/combined
 data = pd.read_csv(user_specified_directory)
 data['TAVG'] = (data['TMIN'] + data['TMAX']) / 2.0
 
-######################## INPUTS ###################################
+######################## INPUTS SECTION #######################################
 # Specify desired grid lengths
 nlat         = 10
 nlong        = 10
 
 # Specify desired feature: TMIN, TMAX, TAVG, SNOW, PRCP, HTDD
-user_feature = 'TMAX'
+user_feature = 'TAVG'
 
 # lat/long indices for test plot, should be between 1 and nlat or nlong
 ilat  = 5
@@ -37,13 +38,7 @@ min_data_pts = 10
 year_start = 2000                                                     # initial year
 year_end   = 2024                                                     # final year
 delta_year = 2                                                        # spacing between years
-nYear      = int(np.ceil((year_end - year_start) / (delta_year)) + 1) # number of years (rounded up)
-test_size  = 0.2                                                      # percentage size of test set
-nTest      = int(np.floor(test_size * nYear))                         # number of years in test set
-years      = np.zeros(nYear)                                          # initialize years array
-for i in range(0,nYear):
-    years[i] = int(year_start + delta_year * i)
-print('years = ', years)
+test_size  = 0.06 #0.2                                                # percentage size of test set
 
 # Specify cross-validation parameters
 ltrain   = 2  # initial length of train set (in element indices, not necessarily years)
@@ -51,15 +46,24 @@ lhorizon = 2  # length of validation set    (in element indices, not necessarily
 iterate  = 1  # step size of validation set (in element indices, not necessarily years)
 
 # Specify regression model
-reg_model = Pipeline([('scale',StandardScaler()),('fit',LinearRegression())])
+reg_model = Pipeline([('fit',LinearRegression())])
 
-# Specify options for (1) creating RMS error csv files for each validation set, (2) creating plots at each validation set, and (3) saving the test data
+# Specify final year for projected deltas, and number of years in projection
+year_end_forecast = 2030
+nYfc = 30
+
+# Specify options for (1) creating RMS error csv files for each validation set, (2) creating plots at each validation step, 
+# (3) plotting the last validation step, (4) saving the test data, and (5) plotting the geographical feature data
 save_errors          = False
 plot_each_validation = False
+plot_last_validation = True
 save_test_data       = False
+plotdata             = False
 
-##################################################################
-def data_clean_split_single_feature(data, min_data_pts, years, nTrain, nHorizon, nlat, nlong, feature):
+######################## END INPUTS SECTION ###################################
+
+####################### FUNCTIONS ##############################################
+def data_clean_split_single_feature(data, min_data_pts, years, nTrain, nHorizon, nlat, nlong, feature, plotdata):
     # Initializing train-test split
     x_train = np.zeros(shape=(nTrain, nlat, nlong))
     x_test  = np.zeros(shape=(nHorizon, nlat, nlong))
@@ -85,6 +89,8 @@ def data_clean_split_single_feature(data, min_data_pts, years, nTrain, nHorizon,
         # Arrays in which model features will be stored
         x_array    = []
         size_array = []
+        lat_array  = []
+        long_array = []
 
         # Keeps track of number of grid points per year that meet the min_data_pts threshold
         yearly_grid_number = 0
@@ -106,6 +112,8 @@ def data_clean_split_single_feature(data, min_data_pts, years, nTrain, nHorizon,
                     # Compute mean model features within this grid cell
                     x_array.append(float(np.mean(df_new[feature])))
                     size_array.append(float(df_new.shape[0]))
+                    lat_array.append(float(np.mean(df_new['LATITUDE']))) #append(float((latitude[lat] + latitude[lat-1]) / 2.0))
+                    long_array.append(float(np.mean(df_new['LONGITUDE']))) #.append(float((longitude[long] + longitude[long-1]) / 2.0))
 
                     # Compute average lat/long coordinates of weather stations in a grid cell
                     lat_grid[lat-1, long-1]  = float(np.mean(df_new['LATITUDE']))
@@ -120,6 +128,28 @@ def data_clean_split_single_feature(data, min_data_pts, years, nTrain, nHorizon,
                         x_train[year, lat-1, long-1] = float(np.mean(df_new[feature]))
                     else:
                         x_test[year-nTrain, lat-1, long-1] = float(np.mean(df_new[feature]))
+
+                        # Plot all solution types for the current year in the loop
+                        # Build new dataframe with grid-cell-averaged data
+
+        if(plotdata and year==len(years)-1):
+            # Create geographical plot of feature data at final year in the current cross-validation step
+            coarse_df = pd.DataFrame.from_dict({'FEATURE' : x_array, 
+                                            'LATITUDE' : lat_array, 
+                                            'LONGITUDE' : long_array,
+                                            'SIZE' : size_array})
+            soln_type='FEATURE'
+            plotzoom  = 3.0
+            fig = px.scatter_map(coarse_df, lat='LATITUDE', lon='LONGITUDE', color=str(soln_type), 
+                                 zoom=plotzoom, width=1000, height=700, size='SIZE', 
+                                 title='Year = ' + str(years[year]) + ', min_data_pts = ' + str(min_data_pts) + ', nlat x nlong = ' + str(nlat) + 'x' + str(nlong))
+            # fig.show()
+            pio.write_image(fig, 'TAVG' + '_plot_' + str(years[year]) + '_minpts_' + 
+                            str(min_data_pts) + '_nlat_x_nlong_' + str(nlat) + 'x' + str(nlong) + '.png')
+
+            # clear plot
+            fig.data   = []
+            fig.layout = {}
 
         # Keep track of number of grid cells per year
         grid_count.append(int(yearly_grid_number))
@@ -196,7 +226,7 @@ def data_test_set(data, min_data_pts, years, nlat, nlong, feature):
     return x_test, lat_grid, long_grid, lat_bin_center, long_bin_center
 
 def forecasting(years, nlat, nlong, nTrain, nHorizon, traindata, reg_model):
-    data_forecast  = np.zeros(shape=(nHorizon, nlat, nlong)) * -1
+    data_forecast  = np.zeros(shape=(nHorizon, nlat, nlong))
     data_coef      = np.zeros(shape=(nlat, nlong))
     data_intercept = np.zeros(shape=(nlat, nlong))
     # Loop over grid cells
@@ -249,12 +279,22 @@ def form_test_set(data, min_data_pts, years, nlat, nlong, user_feature, test_fil
                             'latitude (cell-centered)':lat_center_test.reshape(-1,1)[:,0], 
                             'feature var':xtemp.reshape(-1,1)[:,0]}
             df_test = pd.DataFrame(columns_test)
-            df_test_f = df_test[(df_test != 0).any(axis=1)] # Remove zero'd out rows
+            df_test_f = df_test[(df_test != 0).all(axis=1)] # Remove zero'd out rows
             df_test_f.to_csv(test_file+'_year_'+str(years[i])+'.csv', index=False)
     else:
         print('Not saving test data to file.')
 
     return xtest
+
+################### END FUNCTIONS ##############################################
+
+# Build timeline array
+nYear      = int(np.ceil((year_end - year_start) / (delta_year)) + 1) # number of years (rounded up)
+nTest      = int(np.floor(test_size * nYear))                         # number of years in test set
+years      = np.zeros(nYear)                                          # initialize years array
+for i in range(0,nYear):
+    years[i] = int(year_start + delta_year * i)
+print('years = ', years)
 
 # min/max latitutude and longitude coordinates (set by hand to exclude Hawaii/Alaska)
 latmin  = 25.0
@@ -275,7 +315,8 @@ longlabel = [i+1 for i in range(0,len(longitude)-1)]
 data['LATITUDE_BIN']  = pd.cut(x=data['LATITUDE'],  bins=latitude,  labels=latlabel)
 data['LONGITUDE_BIN'] = pd.cut(x=data['LONGITUDE'], bins=longitude, labels=longlabel)
 
-nCross = int(np.floor(len(years[0:nYear-nTest]) - (ltrain + lhorizon)) / iterate) # number of cross validation sets
+# Number of cross validation set
+nCross = int(np.floor(len(years[0:nYear-nTest]) - (ltrain + lhorizon)) / iterate) 
 if(nCross < 1.):
     print('Number of cross validation sets is invalid')
     print('nYears   = ', len(years[0:nYear-nTest]))
@@ -301,6 +342,10 @@ fs_label  = 15
 fs_tick   = 14
 fs_legend = 13
 
+# Forming the test set
+# Note that the test set and training sets are 
+# created with different functions, even though 
+# the functions appear to be very similar
 xtest = form_test_set(data=data, 
                       min_data_pts=min_data_pts, 
                       years=years[nYear-nTest:nYear], 
@@ -315,7 +360,7 @@ i_cross = 0
 while iE_test <= len(years)-nTest:
     years_new = years[iB_train:iE_test]
 
-    # Form cross validation splits with train data
+    # Form cross validation splits with train data + clean data (remove NaNs)
     xtrain, xvalid, lat_grid, long_grid, lat_center, long_center, gridcount = \
         data_clean_split_single_feature(data=data,
                                         min_data_pts=min_data_pts,
@@ -324,29 +369,31 @@ while iE_test <= len(years)-nTest:
                                         nHorizon=lhorizon,
                                         nlat=nlat,
                                         nlong=nlong,
-                                        feature=user_feature)
-    
+                                        feature=user_feature,
+                                        plotdata=plotdata)
+        
     # Fit model and forecast
     xforecast, xcoef, xintercept  = forecasting(years=years_new, nlat=nlat, nlong=nlong,
-                                                nTrain=ltrain, nHorizon=lhorizon, traindata=xtrain,
+                                                nTrain=ltrain, nHorizon=lhorizon, traindata=xtrain[iB_train:iE_train,:,:],
                                                 reg_model=reg_model)
     
     # Compute errors and save to file (if selected)
     errors[i_cross, :, :] = RMSe(test=xvalid, forecast=xforecast, nH=lhorizon, nLat=nlat, nLong=nlong)
-    for m in range(0,nlat):
-        for k in range(0,nlong):
-            if((xcoef[m,k]==0. or xintercept[m,k]==0.) and (lat_center[m,k]!=0.)):
-                print('xcoef = 0 or xintercept = 0 (min_station_pts requirement not met for some years)')
-                print('xtrain[:,m,k]   = ', xtrain[:,m,k])
-                print('years[iB_train:iE_train]        = ', years_new[iB_train:iE_train])
-                print('xcoef[m,k]      = ', xcoef[m,k])
-                print('xintercept[m,k] = ', xintercept[m,k])
-                print('lat_cent        = ', lat_center[m,k])
-                print('long_cent       = ', long_center[m,k])
-                print('error           = ', errors[i_cross,m,k])
-
 
     if(save_errors and iE_test == len(years)-nTest):
+        # Compute projection to nth year in the future (for demonstration purposes)
+        years_forecast_Project = np.linspace(years_new[len(years_new)-1],year_end_forecast,nYfc)
+        forecast_Project       = np.zeros(shape=(len(years_forecast_Project),nlat,nlong))
+        coef_Project           = np.zeros(shape=(nlat,nlong))
+        intercept_Project      = np.zeros(shape=(nlat,nlong))
+        feature_delta          = np.zeros(shape=(nlat,nlong))
+        for i in range(0,nlat):
+            for j in range(0,nlong):
+                forecast_Project[:,i,j], coef_Project[i,j], intercept_Project[i,j] = \
+                    model_formation(time_train=years_new[0:ltrain].reshape(-1,1), data_train=xtrain[0:ltrain,i,j], model=reg_model, time_forecast=years_forecast_Project[:].reshape(-1,1))
+                feature_delta[i,j] = forecast_Project[nYfc-1,i,j] - forecast_Project[0,i,j]
+        
+        # Build data structure for errors + model properties
         error_file = 'error_ncross_'+str(i_cross+1)+'_minYear_'+str(years[0])+'_maxYear_'+str(years[len(years)-1])+'_var_'+user_feature
         columns = {'longitude (mean)':long_grid.reshape(-1,1)[:,0],
                    'longitude (cell-centered)':long_center.reshape(-1,1)[:,0], 
@@ -354,10 +401,36 @@ while iE_test <= len(years)-nTest:
                    'latitude (cell-centered)':lat_center.reshape(-1,1)[:,0], 
                    'intercept':xintercept.reshape(-1,1)[:,0], 
                    'slope':xcoef.reshape(-1,1)[:,0], 
-                   'RMS errors':errors[i_cross, :, :].reshape(-1,1)[:,0]}
+                   'RMS errors (relative to final validation set, NOT test set)':errors[i_cross, :, :].reshape(-1,1)[:,0],
+                   'feature delta':feature_delta.reshape(-1,1)[:,0]}
         df_errors = pd.DataFrame(columns)
-        df_filter = df_errors[(df_errors != 0).any(axis=1)] # Remove zero'd out rows
+        df_filter = df_errors[(df_errors != 0).all(axis=1)] # Remove any zero entries
         df_filter.to_csv(error_file+'.csv', index=False)
+        # Plot only the final validation step for a specified coordinate
+        if(plot_last_validation):
+            plt.figure(i_cross)
+            plt.plot(years[0:iE_train], xtrain[0:iE_train, ilat, ilong], linewidth=3.0, color='black', label='train data')
+            plt.plot(years[iB_test:iE_test], xforecast[:, ilat, ilong], linewidth=1.5, color='red', linestyle='dashed', label=r'forecast')
+            plt.plot(years[iB_test:iE_test], xvalid[:, ilat, ilong],  linewidth=3.0, color='blue', label='validation data')
+            plt.plot(years_forecast_Project, forecast_Project[:, ilat, ilong], linewidth=3.0, color='magenta', linestyle='dotted', label=r'data projection, $\Delta \mathrm{f}=$' + '{:.3e}'.format(feature_delta[ilat, ilong]))
+            plt.title('lat x long = ' + '{:.3f}'.format(lat_grid[ilat,ilong]) + ' x ' + '{:.3f}'.format(long_grid[ilat,ilong]) + ', RMS error: ' + '{:.3e}'.format(errors[i_cross, ilat, ilong]) + ', ncross = ' + str(i_cross+1))
+            plt.tick_params(axis='x', labelsize=fs_tick)
+            plt.xticks(years[0:iE_test], [int(label) for label in years[0:iE_test]], rotation=45, size=7)
+            plt.xlabel(r'Year', fontsize = fs_label)
+            plt.tight_layout()
+            if(user_feature=='TMAX'):
+                plt.ylabel(r'$T_{\mathrm{max}}$ [degrees Celcius]', fontsize = fs_label)
+            elif(user_feature=='SNOW'):
+                plt.ylabel(r'Snowfall [mm]')
+            elif(user_feature=='PRCP'):
+                plt.ylabel(r'Rainfall [mm]')
+            elif(user_feature=='HTDD'):
+                plt.ylabel(r'Heating degree days')
+            elif(user_feature=='TAVG'):
+                plt.ylabel(r'$T_{\mathrm{avg}}$ [degrees Celcius]')
+            elif(user_feature=='TMIN'):
+                plt.ylabel(r'$T_{\mathrm{min}}$ [degrees Celcius]')
+            plt.legend(fontsize = fs_legend)
 
     # Plot results (if selected)
     if(plot_each_validation):
